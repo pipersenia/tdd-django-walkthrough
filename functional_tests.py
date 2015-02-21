eb98eda7e1c4230b4b8840a7b45b008ebb2c7baf
Aditya Pipersenia <aditya.pipersenia@yahoo.com>
2015-02-21 15:27:41 -0500

    unit test refactored to check each row

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # logon to the site
@@ -46,10 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         whereas the other returns a list, which may be empty.
         '''
         rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                    [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         '''
         Over complicated?
         self.assertTrue(
