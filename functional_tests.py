c21ac55c795ba5940b2878bbbad83b42977af39e
Aditya Pipersenia <aditya.pipersenia@yahoo.com>
2015-02-21 11:52:22 -0500

    Functional test updated to check an input for to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -11,8 +11,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # logon to the site
         self.browser.get('http://localhost:8000')
+
+        # check the title
         self.assertIn('To-Do', self.browser.title)
+        
+        # check the header on the page
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        
+        # Enter todo item 1
+        inputbox.send_keys('Buy peacock feathers')
+        # Send the input
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
